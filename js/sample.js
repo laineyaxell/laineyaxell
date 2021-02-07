500481b3fc903268be5b8c35822a068b086ab458
Lainey Axell <laineyaxell@Laineys-MacBook-Pro-2.local>
2021-02-07 11:22:48 -0600

    trying

--- js/sample.js
+++ js/sample.js
@@ -1,3 +1,19 @@
 function howdy(firstName){
     alert("Howdy " + firstName);
 }
+
+function evalNumber(){
+    var inputValue = prompt("Enter a 5 digit number")
+    if (isNaN(inputValue)) {
+        alert(inputValue + " is not a number.")
+    } else if (inputValue % 2 == 0) {
+        alert(inputValue + " is an even number.")
+    } else {
+        alert(inputValue + " is an odd number.")
+    }
+}
+
+function getTime(){
+var today = New Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+}
